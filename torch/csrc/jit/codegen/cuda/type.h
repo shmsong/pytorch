58c851599ee693f09118cbf160a7359ae1214b63
Kevin Stephano <kevin.stephano@gmail.com>
2020-05-07 20:38:07 -0700

    Refactor Cast Op handling.

--- torch/csrc/jit/codegen/cuda/type.h
+++ torch/csrc/jit/codegen/cuda/type.h
@@ -43,7 +43,7 @@ enum class UnaryOpType {
   Asin,
   Atan,
   Atanh,
-  // Cast,
+  Cast,
   Ceil,
   Cos,
   Cosh,
@@ -51,11 +51,9 @@ enum class UnaryOpType {
   Expm1,
   Erf,
   Erfc,
-  FloatToHalf,
   Floor,
   Frac,
   Gelu,
-  HalfToFloat,
   Lgamma,
   Log,
   Log10,
@@ -121,7 +119,6 @@ enum class ParallelType {
 
 ValType promote_type(const ValType& t1, const ValType& t2);
 DataType promote_type(const DataType& t1, const DataType& t2);
-c10::optional<UnaryOpType> cast_type(const DataType& t1, const DataType& t2);
 bool is_logical_op(const BinaryOpType& bot);
 
 DataType aten_to_data_type(const at::ScalarType& scalar_type);
@@ -139,6 +136,8 @@ std::string stringifyThreadSize(const ParallelType);
 
 TORCH_CUDA_API c10::optional<std::string> inline_op_str(const UnaryOpType);
 TORCH_CUDA_API c10::optional<std::string> inline_op_str(const BinaryOpType);
+
+TORCH_CUDA_API c10::optional<std::string> cast_func_str(const std::pair<DataType,DataType>&);
 
 } // namespace fuser
 } // namespace jit
