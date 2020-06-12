e246f1a4fe4cc0219d7d223a7909c0ce3fe73763
Christian Sarofeen <csarofeen@nvidia.com>
2020-06-12 13:43:23 -0400

    Minor cleanup. (#79)

--- test/cpp/jit/test_gpu.cpp
+++ test/cpp/jit/test_gpu.cpp
@@ -1,4 +1,4 @@
-// #if defined(USE_CUDA)
+#if defined(USE_CUDA)
 #include <test/cpp/jit/test_base.h>
 
 #include <torch/csrc/jit/codegen/cuda/arith.h>
@@ -4058,4 +4058,4 @@ void testGPU_FusionZeroDimReduction() {
 
 } // namespace jit
 } // namespace torch
-// #endif // #if defined(USE_CUDA)
+#endif // #if defined(USE_CUDA)
