cdcc289b7b918a493cd86ab957209f2b1a30a606
Christian Sarofeen <csarofeen@nvidia.com>
2020-06-17 19:17:30 -0400

    Quick fixes (#92)
    * Remove extra new lines in fusion printMath.
    
    * Fix bug in predicate generation.
    
    * Add fusion.printKernel()
    
    * Fix potential segfault in unroll pass.

--- torch/csrc/jit/codegen/cuda/fusion.cpp
+++ torch/csrc/jit/codegen/cuda/fusion.cpp
@@ -343,20 +343,6 @@ void Fusion::print() {
   std::cout << "}\n";
 }
 
-void Fusion::printValuesMap() {
-  IRPrinter ir_printer(std::cout);
-  ir_printer.follow_val_map = false;
-  std::cout << "\nValues map\n";
-  std::cout << "--------------------\n";
-  for (const auto& kv : values_map_) {
-    ir_printer.handle(kv.first);
-    std::cout << " -> ";
-    ir_printer.handle(kv.second);
-    std::cout << "\n";
-  }
-  std::cout << "--------------------\n\n";
-}
-
 void Fusion::printKernel() {
   FusionGuard fg(this);
   GPULower lower(this);
@@ -366,7 +352,7 @@ void Fusion::printKernel() {
 void Fusion::printMath() {
   FusionGuard fg(this);
   for (auto expr : exprs(true))
-    std::cout << expr << std::endl;
+    std::cout << expr;
 }
 
 void Fusion::printTransforms() {
