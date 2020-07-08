89698e898c65f5487a6aedf343cccdbe18fae461
Jie <jiej@nvidia.com>
2020-07-08 12:23:53 -0700

    silencing clang-tidy trial 2

--- torch/csrc/jit/codegen/cuda/scheduler.cpp
+++ torch/csrc/jit/codegen/cuda/scheduler.cpp
@@ -248,8 +248,8 @@ inline int last_pow2(int n) {
   n |= (n >> 1);
   n |= (n >> 2);
   n |= (n >> 4);
-  n |= (n >> 8);
-  n |= (n >> 16);
+  n |= (n >> 8); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+  n |= (n >> 16); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
   return std::max(1, n - (n >> 1));
 }
 
