ba2b9f6bcc4304388ce5781579d145e2f1d4548d
Jie <jiej@nvidia.com>
2020-08-11 03:41:17 -0700

    adding more comments

--- torch/csrc/jit/codegen/cuda/kernel_cache.h
+++ torch/csrc/jit/codegen/cuda/kernel_cache.h
@@ -63,35 +63,60 @@ class FusionExecutorCache {
   // fusion executor is taking the ownership of `fusion`;
   FusionExecutorCache(std::unique_ptr<Fusion>&& fusion, at::Device device);
 
+  // Execute fusion graph with given inputs, create `FusionExecutor` as needed;
   std::vector<at::Tensor> runFusionWithInputs(
       const at::ArrayRef<IValue>& inputs);
 
  private:
+  // device_ where compiled binaries are loaded on & inputs are expected to
+  // reside;
   at::Device device_;
+
+  // original un-scheduled `Fusion`;
   std::unique_ptr<Fusion> fusion_;
+
+  // TODO: placeholder that will be updated;
   std::vector<std::unique_ptr<FusionExecutor>> fusion_executor_cache_;
 };
 
 class GraphCache {
  public:
+  // TODO: we should probably change shared_ptr to unique_ptr, as we want to
+  //       claim the ownership of the computational graph.
+  // create GraphCache on a given graph;
+  // Note: if run with profiling executor, we'll try to generete a kernel with
+  // profiling information at this moment.
   GraphCache(std::shared_ptr<Graph> graph);
 
+  // execute graph with given inputs.
   std::vector<at::Tensor> runGraphWithInputs(
       const at::ArrayRef<IValue>& inputs);
 
  private:
   // TODO: place holder with naive implementation for now.
+  // structure use to mark the compatibility of each FusionExecutorCache;
+  // We also have `input_permutation_` & `output_permutation_` used to
+  // facilitate dimension coalescing per stride order.
   struct InputsRequirement {
+    // target device
     c10::optional<at::Device> device_;
-    at::DimVector input_permutation_;
-    at::DimVector output_permutation_;
     // TODO: TensorTypePtr is not very easy to work with.
     // c10::nullopt to take place of non-tensor type;
     std::vector<c10::optional<at::TensorTypePtr>> vec_optional_ttp;
 
+    // common permutation order used for dimension coalescing;
+    at::DimVector input_permutation_;
+    at::DimVector output_permutation_;
+
+    // construct InputsRequirement from `Graph`, this is used for constructing
+    // `GraphCache` entry using profiling record
     InputsRequirement(
         const std::shared_ptr<Graph>& graph,
         const std::vector<size_t>& reduction_axes);
+
+    // construct InputsRequirement from live input feeds, this is used to handle
+    // run-time inputs to: 1. search for compatible entry; 2. insert new entry
+    // in case of a cache miss.
     InputsRequirement(
         const at::ArrayRef<IValue>& inputs,
         const std::vector<size_t>& reduction_axes);
@@ -99,9 +124,14 @@ class GraphCache {
     // bool operator==(const InputsRequirement& other);
     bool complyWith(const InputsRequirement& expect);
 
+    // helper function used at run-time to check whether a common permutation is
+    // present, this is used to take the short-cut to skip permutation logic.
     bool requiresPermutation();
   };
 
+  // construct FusionExecutorCache per InputsRequirement.
+  // This function makes sure that we properly insert both `input_stacks_` and
+  // `fe_cache_` at the same time.
   FusionExecutorCache* createFusionExecutorCache(
       const InputsRequirement& input_stack);
 
