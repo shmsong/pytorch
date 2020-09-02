1efb7b2aa0842bb226ac7924a2121c95a3592722
Lemo <lemo1234@gmail.com>
2020-09-02 15:01:23 -0700

    StatefulExpressionEvaluator_V3

--- torch/csrc/jit/codegen/cuda/expr_evaluator.h
+++ torch/csrc/jit/codegen/cuda/expr_evaluator.h
@@ -15,6 +15,73 @@ namespace torch {
 namespace jit {
 namespace fuser {
 
+// Encapsulates a set of value bindings on top of a Fusion IR
+// (used to provide known values to ExpressionEvaluator)
+//
+// NOTE: currently it only supports Int values
+//
+class TORCH_CUDA_API EvaluationContext {
+ public:
+  explicit EvaluationContext(Fusion* fusion) : fusion_(fusion) {}
+
+  // Set the concrete value for a Int*
+  void bind(const Val* value, Int::ScalarType concrete_value);
+
+  // Retrieves the concrete value, or nullopt if not set
+  c10::optional<Int::ScalarType> concreteValue(const Val* value) const;
+
+  Fusion* fusion() const {
+    return fusion_;
+  }
+
+  // Debugging helper, prints all the currently set values
+  void print() const;
+
+ private:
+  std::unordered_map<const Val*, Int::ScalarType> bindings_;
+  Fusion* fusion_ = nullptr;
+};
+
+// Evaluates expressions in a Fusion IR, using the passed in
+// context (EvaluationContext) to query for concrete_values. The
+// evaluation context may override concrete values in the IR as well.
+class TORCH_CUDA_API ExpressionEvaluator : private IterVisitor {
+ public:
+  // Returns the result of the specified expression, or nullopt if
+  // the result cannot be evaluated
+  static c10::optional<Int::ScalarType> evaluate(
+      Val* val,
+      const EvaluationContext* context);
+
+ private:
+  explicit ExpressionEvaluator(const EvaluationContext* context)
+      : context_(context) {}
+
+  ~ExpressionEvaluator() override = default;
+
+  c10::optional<Int::ScalarType> value(const Statement* stmt) const;
+
+  using IterVisitor::handle;
+
+  void handle(NamedScalar*) override;
+  void handle(Int*) override;
+  void handle(UnaryOp*) override;
+  void handle(BinaryOp*) override;
+
+  // TODO(kir): remove this
+  void handle(kir::NamedScalar*) override;
+  void handle(kir::Int*) override;
+  void handle(kir::UnaryOp*) override;
+  void handle(kir::BinaryOp*) override;
+
+ private:
+  const EvaluationContext* context_ = nullptr;
+  
+  std::unordered_map<const Statement*, Int::ScalarType> values_;
+};
+
+//==============================================================================
+
 class TORCH_CUDA_API StatefulExpressionEvaluator_V1 : private OptOutDispatch {
  public:
   explicit StatefulExpressionEvaluator_V1(Fusion* fusion) : fusion_(fusion) {}
@@ -78,6 +145,8 @@ class TORCH_CUDA_API StatefulExpressionEvaluator_V1 : private OptOutDispatch {
   c10::optional<Int::ScalarType> maybeHandle(Val*);
 };
 
+//==============================================================================
+
 class TORCH_CUDA_API StatefulExpressionEvaluator_V2 : private IterVisitor {
  public:
   explicit StatefulExpressionEvaluator_V2(Fusion* fusion) : fusion_(fusion) {}
@@ -122,7 +191,33 @@ class TORCH_CUDA_API StatefulExpressionEvaluator_V2 : private IterVisitor {
   c10::optional<Int::ScalarType> maybeHandle(Val*);
 };
 
-using StatefulExpressionEvaluator = StatefulExpressionEvaluator_V2;
+//==============================================================================
+
+class TORCH_CUDA_API StatefulExpressionEvaluator_V3 : private IterVisitor {
+ public:
+  explicit StatefulExpressionEvaluator_V3(Fusion* fusion) : context_(fusion) {}
+
+  Fusion* fusion() const {
+    return context_.fusion();
+  }
+
+  void safeBind(
+      Val* value,
+      Int::ScalarType concrete_value,
+      GpuLower* lower = nullptr);
+
+  // Returns value if found in mapping, otherwise returns c10::nullopt
+  c10::optional<Int::ScalarType> getValue(Val* value);
+
+  // Checks if value is already infered, returns infered value if so, otherwise
+  // runs traversal on value. Warning: should not be called in traversal.
+  c10::optional<Int::ScalarType> inferValue(Val* value);
+
+ private:
+  EvaluationContext context_;
+};
+
+using StatefulExpressionEvaluator = StatefulExpressionEvaluator_V3;
 
 } // namespace fuser
 } // namespace jit
